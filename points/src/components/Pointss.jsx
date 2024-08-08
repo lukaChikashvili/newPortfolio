466eacb43b0edef2ef0ca7a9411af5784a70c2fc
luka chikashvili <lukachikashvili2020@gmail.com>
2024-08-08 18:09:16 +0400

    add texture to particle

--- points/src/components/Pointss.jsx
+++ points/src/components/Pointss.jsx
@@ -2,18 +2,25 @@ import React, { useRef } from 'react'
 import * as THREE from 'three'
 import vertex from '../shaders/particles/vertex.glsl';
 import fragment from '../shaders/particles/fragment.glsl';
+import { useTexture } from '@react-three/drei';
 
 
 
 const Particles = () => {
+
+  // load image
+  const image = useTexture('./images.jpg');
+
      const particleRef = useRef();
 
-     const particleGeometry = new THREE.PlaneGeometry(10, 10, 32, 32);
+     const particleGeometry = new THREE.PlaneGeometry(10, 10, 128, 128);
 
      const particleMaterial = new THREE.ShaderMaterial({
       vertexShader: vertex,
-      fragmentShader: fragment
-
+      fragmentShader: fragment,
+      uniforms: {
+        uTexture: new THREE.Uniform(image)
+      }
      });
 
      return (
