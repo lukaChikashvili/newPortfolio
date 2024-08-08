7e2b4ca0639a52b39494dcb9327fb740d8d68ee0
luka chikashvili <lukachikashvili2020@gmail.com>
2024-08-09 00:02:53 +0400

    add 3d models with particles

--- points/src/components/Pointss.jsx
+++ points/src/components/Pointss.jsx
@@ -3,27 +3,49 @@ import * as THREE from 'three';
 import vertex from '../shaders/particles/vertex.glsl';
 import fragment from '../shaders/particles/fragment.glsl';
 import { useGLTF } from '@react-three/drei';
-import { useFrame } from '@react-three/fiber';
 
 const Particles = () => {
   const particleRef = useRef();
-  const model = useGLTF('./soccer_ball.glb');
+  const model = useGLTF('./models1.glb');
 
   const particleGeometry = new THREE.BufferGeometry();
 
-  const positions = [];
-
   useEffect(() => {
+    
+    const positions = model.scene.children.map((child) => child.geometry.attributes.position);
 
-      
-  model.scene.traverse((child) => {
-    if(child.isMesh) {
-      const { position } = child.geometry.attributes;
+    let maxCount = 0;
 
-      console.log(position)
-    }
-  })
-   
+    for(const position of positions) {
+      if(position.count > maxCount) {
+        maxCount = position.count;
+      }
+     }
+    
+     const newPositions = [];
+
+    for(const position of positions) {
+        const originalArray = position.array;
+        const newArray = new Float32Array(maxCount * 3);
+        
+        for(let i = 0; i < maxCount; i++) {
+          const i3 = i * 3;
+
+          if(i3 < originalArray.length) {
+            newArray[i3 + 0] = originalArray[i3 + 0];
+            newArray[i3 + 1] = originalArray[i3 + 1];
+            newArray[i3 + 2] = originalArray[i3 + 2];
+          }else {
+            newArray[i3 + 0] = 0;
+            newArray[i3 + 1] = 0;
+            newArray[i3 + 2] = 0;
+          }
+        }
+
+        newPositions.push(new THREE.Float32BufferAttribute(newArray, 3));
+
+       particleGeometry.setAttribute('position', newPositions[1])
+     }
   }, [model]);
 
   const particleMaterial = new THREE.ShaderMaterial({
